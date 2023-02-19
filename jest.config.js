37c35e99a1fe96a45730edc81cfc20fc89fa4f0e
Muthukumar Arumugam <muthukumar438@gmail.com>
2023-02-20 03:29:42 +0530

    Minor bug fixes

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
-const { pathsToModuleNameMapper } = require('ts-jest');
-const { compilerOptions } = require('./tsconfig.json');
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
 
 module.exports = {
   preset: 'ts-jest',
